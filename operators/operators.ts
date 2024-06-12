f0447ae164c6cf58b4393f96e55064b458f35095
Rohinth3 <rohinth0304@gmail.com>
2024-06-12 12:15:34 +0530

    hbdjc

---
+++ operators/operators.ts
@@ -0,0 +1,18 @@
+// let x = 5
+// let y = 7
+
+// console.log('1' + "Rohinth")    o/p : 1Rohinth
+
+
+// let x = 5
+// let y = 7
+// let z = 8              
+
+// console.log (!x>y && (y<z || z>x))           o/p : False 
+
+
+// let a = 5
+// let b = 7
+// let c = 8
+
+// console.log (!(a>b && (b<c || c>a)))        o/p : True 
