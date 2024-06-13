25a74bb1ff41974041b0ba0fa5ddf6b2e1431a07
Rohinth3 <rohinth0304@gmail.com>
2024-06-13 19:18:24 +0530

    fjs

---
+++ javaScript/flowcontrol.js
@@ -0,0 +1,55 @@
+//FLOW CONTROL STATEMENT
+
+//IF ELSE ELSE IF 
+
+
+ let name = "santhosh"
+
+ if (name.includes('t'),name.includes('o')){
+    console.log("The name has t and o character")
+ } else{
+    console.log("Not a character")
+ }
+
+
+// let x =14
+
+// if (x <17){
+//     console.log("x is less than 14")
+// }else if (x == 15){
+//     console.log(' x is equal to 14')
+// }else if(x>15){
+//     console.log('x is greater than 14')
+// }else {
+//     console.log('not applicable')
+// }
+
+
+//check the 3 index number is x
+
+// let userName = "wa xcream"
+
+// console.log(userName[3])
+
+// if (userName[3]){
+//     console.log("The third number is x")
+// }else {
+//     console.log("Not applicable")
+// }
+
+
+// let a =14
+// let b =12
+// let c =13
+
+// if(a>b,a>c){
+//     console.log('a is greater than b and c')
+// }else if (b>a,b>c){
+//     console.log('b is greater than c and a')
+// }else if (c>a,c>b){
+//     console.log('c is greater than b and a')
+// }else{
+//     console.log('Not applicable')
+// }
+
+// ---------------------------------------------------------------------------------------------------------------------
--- javaScript/stringhandling.js
+++ javaScript/stringhandling.js
@@ -1,4 +1,27 @@
 
-let loginName = "Rohinth"
+// // INDEXING
+// let loginName = "Rohinth"
 
-console.log(loginName[0])
+// console.log(loginName[4]+loginName[3])
+
+// ----------------------------------------------------------------
+
+// SLICING AND RANGING NEGATIVE
+// let userName = "Rohinth"
+
+// console.log(userName.slice(-4,-2))
+// //ranging
+// console.log(userName.slice(-4))  
+
+//-------------------------------------------------------------------
+
+// //substring reverse used no negative indexing used
+// let userName = "Rohinth-Murugan"
+
+// console.log(userName.substring(14,5))
+
+// // not working using substring
+// console.log(userName.substring(-2,-4))
+
+// ----------------------------------------------------------------------
+
