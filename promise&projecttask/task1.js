343be2f7da85f770d2d093782d5bee0a1b3d7d76
Rohinth3 <rohinth0304@gmail.com>
2024-09-02 20:13:46 +0530

    fnjk

--- promise&projecttask/task1.js
+++ promise&projecttask/task1.js
@@ -7,29 +7,56 @@
 // update student some unique id or email id
 // delete student with some unique id or email id
 
-const MainDiv = document.getElementById('mainDiv')
+// const MainDiv = document.getElementById('mainDiv')
 
 const butDiv = document.getElementById('buttonDiv')
-
 const NameDiv = document.getElementById('nameDiv')
 const AgeDiv = document.getElementById('ageDiv')
 const EmailDiv = document.getElementById('emailDiv')
 const DeptDiv = document.getElementById('deptDiv')
 
+// const butDiv1 = document.getElementById('buttonDiv1')
+// const updateName = document.getElementById('updateName')
+// const updateAge = document.getElementById('updateAge')
+// const updateEmail = document.getElementById('updateEmail')
+// const updateDept = document.getElementById('updateDept')
 
 class UserRegistration{
+    /// constructor automatically assign
     constructor(){
-        this.user = []
+        this.user = [
+            // {                              like this it will perform
+            //     name: "rohinth",
+            // }
+            // {
+            //     name : "suresh",
+            // }
+        ]
     }
 
     adduser(name,age,email,dept){
-        console.log(NameDiv,AgeDiv,EmailDiv,DeptDiv)
+        let userObj = {
+            "name"  : NameDiv.value,
+            "age"   : AgeDiv.value,
+            "email" : EmailDiv.value,
+            "dept"  : DeptDiv.value
+        }
+        // console.log(NameDiv.value,AgeDiv.value,EmailDiv.value,DeptDiv.value)
+        // console.log(userObj)
+
+        this.user.push(userObj)
+        console.log(this.user)
+        NameDiv.value = ''
+        AgeDiv.value = ''
+        EmailDiv.value = ''
+        DeptDiv.value = ''
+
     }
     getuser(){
 
     }
     updateuser(){
-
+        
     }
 
 }
@@ -37,5 +64,8 @@ obj = new UserRegistration();
 
 butDiv.addEventListener("click",()=>{
     obj.adduser(NameDiv,AgeDiv,EmailDiv,DeptDiv)
+   
 })
+
+
     
